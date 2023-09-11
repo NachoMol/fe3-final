1c5321a3baccb6bea6aecaa5d8c1e220d821cdc0
NachoMol <ignaciotmolina@gmail.com>
2023-09-11 19:41:14 -0300

    agregando logos al footer (modificar a logos negros)

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,6 +6,10 @@ const Footer = () => {
     <footer>
         <p>Powered by</p>
         <img src="/images/DH.png" alt='DH-logo' />
+        <img src="/images/ico-facebook.png" alt='facebook-logo' />
+        <img src="/images/ico-instagram.png" alt='instagram-logo' />
+        <img src="/images/ico-tiktok.png" alt='tiktok-logo' />
+        <img src="/images/ico-whatsapp.png" alt='whatsapp-logo' />
     </footer>
   )
 }
